f2e9e2015337c1ffafcf59342c2bd688ccbb4ea8
stupendous-web <topher@stupendousweb.com>
2022-07-10 15:43:43 -0700

    Think twice, code once.

--- pages/api/capture.js
+++ pages/api/capture.js
@@ -12,6 +12,8 @@ export default async function handler(req, res) {
   try {
     await prisma.pageview.create({
       data: {
+        site: req.body.site || null,
+        anonymousId: req.body.anonymousId || null,
         path: req.body.path || null,
         referrer: req.body.referrer || null,
         height: req.body.height || null,
