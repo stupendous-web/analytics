39c36acb83aa25333aac6495123e513e05b93053
stupendous-web <topher@stupendousweb.com>
2022-07-10 10:54:50 -0700

    Think twice, code once.

--- pages/api/capture.js
+++ pages/api/capture.js
@@ -1,8 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import NextCors from "nextjs-cors";
 
 const prisma = new PrismaClient();
 
 export default async function handle(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
   const pageview = await prisma.pageview.create({
     data: {
       path: "/",
