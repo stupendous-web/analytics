5730734c6adf18be3015dfe51ca872ff3fac2126
stupendous-web <topher@stupendousweb.com>
2022-07-10 15:54:08 -0700

    Think twice, code once.

--- pages/api/capture.js
+++ pages/api/capture.js
@@ -3,20 +3,15 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  try {
-    await prisma.pageview.create({
-      data: {
-        site: req.body.site,
-        anonymousId: req.body.anonymousId,
-        path: req.body.path,
-        referrer: req.body.referrer,
-        height: req.body.height,
-        width: req.body.width,
-      },
-    });
-  } catch (error) {
-    res.status(500).send(error);
-    throw error;
-  }
+  await prisma.pageview.create({
+    data: {
+      site: req.body.site,
+      anonymousId: req.body.anonymousId,
+      path: req.body.path,
+      referrer: req.body.referrer,
+      height: req.body.height,
+      width: req.body.width,
+    },
+  });
   res.json("Good things come to those who wait.");
 }
