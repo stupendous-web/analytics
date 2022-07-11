1397fbe724797cc00323271fada72f6d12f5bfa8
stupendous-web <topher@stupendousweb.com>
2022-07-10 20:22:27 -0700

    Think twice, code once.

--- pages/[site].js
+++ pages/[site].js
@@ -1,17 +1,46 @@
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 export default function Site() {
   const router = useRouter();
   const { site } = router.query;
 
-  const [pageviews, setPageviews] = useState();
+  const [referrers, setReferrers] = useState();
   useEffect(() => {
+    if (!router.isReady) return;
     axios.get("/api/pageviews/" + site).then((response) => {
-      console.log(response.data);
+      setReferrers(response.data.referrers);
     });
-  });
+  }, [router.isReady]);
 
-  return <h1>{site}</h1>;
+  return (
+    <>
+      {" "}
+      <div className={"uk-section"} uk-height-viewport={""}>
+        <div className={"uk-container uk-container-expand"}>
+          {site && <h1>@{site}</h1>}
+          <h2>Top Referrers</h2>
+          <table className="uk-table uk-table-striped uk-table-small">
+            <thead>
+              <tr>
+                <th>Location</th>
+                <th>Pageviews</th>
+              </tr>
+            </thead>
+            <tbody>
+              {referrers?.map((referrer, key) => {
+                return (
+                  <tr key={key}>
+                    <td>{referrer.path}</td>
+                    <td>1</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
--- pages/api/pageviews/[site].js
+++ pages/api/pageviews/[site].js
@@ -3,6 +3,14 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handle(req, res) {
-  const pageviews = await prisma.pageview.findMany();
-  res.json(pageviews);
+  const referrers = await prisma.pageview.groupBy({
+    by: ["referrer"],
+    where: {
+      site: {
+        contains: "stupendous-web",
+      },
+    },
+  });
+  console.log(referrers);
+  res.json({ referrers: referrers });
 }
